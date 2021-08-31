0d10d3cf0ddec4b3f3e62427410d0c8e03b6cb91
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-31 14:16:40 -0300

    DTOs que serão usados no projeto

--- src/main/java/com/piter/videoapi/dto/VideoDTO.java
+++ src/main/java/com/piter/videoapi/dto/VideoDTO.java
@@ -1,5 +1,18 @@
 package com.piter.videoapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VideoDTO {
+	
+	private String descricao;
+	
+	private String titulo;
+	
+	private String url;
 
 }
