c9711ee7aad6624f56d7e7057874a82fd7fdc766
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-31 14:02:06 -0300

    Configurando o Swagger

---
+++ src/main/java/com/piter/videoapi/config/swagger/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.piter.videoapi.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.piter.videoapi.controller"))
+				.paths(PathSelectors.any()).build()
+				.apiInfo(apiInfo())
+				.tags(new Tag("Videos", "Métodos para gerenciamento dos cadastros dos vídeos"));
+	}
+	
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+                .title("Videos API")
+                .description("Api para cadastro, listagens e gerencimanto de links de videos.")
+                .version("1.0.0")
+                .build();
+	}
+
+}
