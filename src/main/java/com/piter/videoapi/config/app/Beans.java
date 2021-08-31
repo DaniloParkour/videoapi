972b6d127535cd186cb0ab6894b5f53ec62f83f8
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-31 13:56:56 -0300

    ModelMapper configurado injeção de dependencias

--- src/main/java/com/piter/videoapi/config/app/Beans.java
+++ src/main/java/com/piter/videoapi/config/app/Beans.java
@@ -1,5 +1,15 @@
 package com.piter.videoapi.config.app;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Beans {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
