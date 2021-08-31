03f1f291e88dfed31314bcce09ae3c7b75ec5b2d
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-31 14:16:22 -0300

    Entidade - Classe Model

--- src/main/java/com/piter/videoapi/model/Video.java
+++ src/main/java/com/piter/videoapi/model/Video.java
@@ -1,5 +1,30 @@
 package com.piter.videoapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "video")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Video {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String descricao;
+	
+	private String titulo;
+	
+	private String url;
 
 }
