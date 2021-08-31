4209344bae35bbea432114b0d11aa5fecfc90576
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-31 17:05:48 -0300

    Campor Nao Nulos SaveVideoDTO

--- src/main/java/com/piter/videoapi/dto/SaveVideoDTO.java
+++ src/main/java/com/piter/videoapi/dto/SaveVideoDTO.java
@@ -3,16 +3,20 @@ package com.piter.videoapi.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SaveVideoDTO {
 	
+	@NonNull
 	private String descricao;
 	
+	@NonNull
 	private String titulo;
 	
+	@NonNull
 	private String url;
 
 }
