94fefd77256b0b1714077b8aea332937697b6429
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-31 14:18:56 -0300

    Definição do Repository

--- src/main/java/com/piter/videoapi/repository/VideoRepository.java
+++ src/main/java/com/piter/videoapi/repository/VideoRepository.java
@@ -1,5 +1,11 @@
 package com.piter.videoapi.repository;
 
-public interface VideoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.piter.videoapi.model.Video;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, Long> {
 
 }
