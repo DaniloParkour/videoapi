3ba5d4ede87f7d727264b24caa633049d87ec958
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-31 14:08:52 -0300

    Implementacao Classe Modelo Para Respostas

--- src/main/java/com/piter/videoapi/mapper/requests/ResponseModel.java
+++ src/main/java/com/piter/videoapi/mapper/requests/ResponseModel.java
@@ -1,5 +1,38 @@
 package com.piter.videoapi.mapper.requests;
 
-public class ResponseModel {
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Singular;
+
+@Data
+@Builder(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@JsonInclude(Include.NON_EMPTY)
+public class ResponseModel<T> {
+	
+	@NonNull
+	private String status;
+	private String message;
+	private T data;
+	private Long totalElements;
+	private Integer totalPages;
+	@Singular("item")
+	private List<T> list;
+	
+	@Singular("error")
+	private Map<Object, Object> errors;
 
 }
