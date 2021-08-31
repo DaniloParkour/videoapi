724078b1f1b07ac125b57473339be18c9e64fe63
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-31 14:05:02 -0300

    Mapeamento Generico Entidade para DTO e listas

--- src/main/java/com/piter/videoapi/mapper/GenericMapper.java
+++ src/main/java/com/piter/videoapi/mapper/GenericMapper.java
@@ -1,5 +1,31 @@
 package com.piter.videoapi.mapper;
 
-public class GenericMapper {
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class GenericMapper  implements IGenericMapper {
+	
+	private final ModelMapper modelMapper;
+
+    @Override
+    public <T> T toObject(Object obj, Class<T> clazz) {
+        if(Objects.isNull(obj)) return null;
+        return modelMapper.map(obj, clazz);
+    }
+
+    @Override
+    public <T> List<T> toList(List<?> list, Class<T> clazz) {
+        if(Objects.isNull(list) || list.isEmpty()) return Collections.emptyList();
+        return list.stream().map(obj -> toObject(obj, clazz)).collect(Collectors.toList());
+    }
 
 }
--- src/main/java/com/piter/videoapi/mapper/IGenericMapper.java
+++ src/main/java/com/piter/videoapi/mapper/IGenericMapper.java
@@ -1,5 +1,10 @@
 package com.piter.videoapi.mapper;
 
-public class IGenericMapper {
+import java.util.List;
 
+public interface IGenericMapper {
+	
+    <T> T toObject(Object obj, Class<T> clazz);
+    <T> List<T> toList(List<?> list, Class<T> clazz);
+    
 }
